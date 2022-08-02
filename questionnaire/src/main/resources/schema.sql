bdfb8bed91a55cd2655335024504d6a979de07a4
yuyayamauchi <yamauchi@shiny-pork-works.co.jp>
2022-08-02 20:08:52 +0900

    08/02 20:00

--- questionnaire/src/main/resources/schema.sql
+++ questionnaire/src/main/resources/schema.sql
@@ -1,11 +1,45 @@
-CREATE TABLE IF NOT EXISTS query_answer (
-    id VARCHAR(8) PRIMARY KEY ,
-    like_meat VARCHAR(1) not null ,
-    like_veg VARCHAR(4)not null ,
-    like_idol VARCHAR(64)not null
+CREATE TABLE IF NOT EXISTS vegetables (
+    vegetable_id int not null PRIMARY KEY ,
+    name VARCHAR(255) not null ,
+    is_deleted tinyint ,
+    created_date datetime not null ,
+    update_date datetime
 );
 
-CREATE TABLE IF NOT EXISTS admin_info (
-    admin_id VARCHAR(16) PRIMARY KEY not null ,
-    admin_pw VARCHAR(16) not null
+CREATE TABLE IF NOT EXISTS meats (
+    meat_id int not null PRIMARY KEY ,
+    name VARCHAR(255) not null ,
+    is_deleted tinyint ,
+    created_date datetime not null ,
+    update_date datetime
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+    answer_id int not null PRIMARY KEY ,
+    meat_id int not null ,
+    idol_name VARCHAR(64) not null ,
+    is_deleted tinyint ,
+    create_date datetime not null ,
+    update_date datetime ,
+    CONSTRAINT FOREIGN KEY (meat_id) REFERENCES meats(meat_id)
+);
+
+CREATE TABLE IF NOT EXISTS answervegetables (
+    answervegetable_id int not null PRIMARY KEY ,
+    vegetable_id int not null ,
+    answer_id int not null ,
+    is_deleted tinyint ,
+    create_date datetime not null ,
+    update_date datetime ,
+    FOREIGN KEY (vegetable_id) REFERENCES vegetables(vegetable_id)
+    FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+    admin_id int not null ,
+    name VARCHAR(255) not null ,
+    password VARCHAR(255) not null ,
+    is_deleted tinyint ,
+    created_date datetime not null ,
+    update_date datetime
 );
