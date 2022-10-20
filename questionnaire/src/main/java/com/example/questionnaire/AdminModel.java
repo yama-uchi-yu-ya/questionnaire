16e98ee9e1d4006f273d01cb4dda7004f63e9709
yuyayamauchi <yamauchi@shiny-pork-works.co.jp>
2022-10-20 22:10:52 +0900

    10/20 22:10

--- questionnaire/src/main/java/com/example/questionnaire/AdminModel.java
+++ questionnaire/src/main/java/com/example/questionnaire/AdminModel.java
@@ -7,7 +7,7 @@ public class AdminModel {
     @NotNull(message = "IDが違います")
     @Size(min = 8, max = 16, message = "IDが違います")
     @Pattern(regexp = "^[A-Za-z0-9]+$", message = "IDが違います")
-    private String name;
+    private String username;
 
     @NotNull(message = "パスワードが違います")
     @Size(min = 8, max = 16, message = "パスワードが違います")
@@ -15,11 +15,11 @@ public class AdminModel {
     private String password;
 
     public String getName() {
-        return this.name;
+        return this.username;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.username = name;
     }
 
     public String getPassword() {
--- questionnaire/src/main/java/com/example/questionnaire/WebSecurityConfig.java
+++ questionnaire/src/main/java/com/example/questionnaire/WebSecurityConfig.java
@@ -1,7 +1,54 @@
 package com.example.questionnaire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated();
+
+        http.formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/sign_in")
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .successForwardUrl("/view")
+            .failureUrl("/login?error")
+            .permitAll();
+
+        http.logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login?logout")
+            .permitAll();
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
 }
+
